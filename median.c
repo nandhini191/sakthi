d8377f8e26039b4f10831822f58ab964a6ae43cd
nandhini191 <30830172+nandhini191@users.noreply.github.com>
2018-01-23 16:03:21 +0530

    Create median.c

---
+++ median.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+int num,i,b,median;
+int a[5];
+printf("enter the array values");
+scanf("%d",array);
+for(i=0;i<=num;i++)
+{
+if(num==median)
+{
+printf("print the number");
+}
+else
+{
+return 0;
+}
+}
+}

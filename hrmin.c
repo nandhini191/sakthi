e80d96470fac74a085c29e6cee7c287035b8ed70
nandhini191 <30830172+nandhini191@users.noreply.github.com>
2018-01-23 16:46:27 +0530

    Create hrmin.c

---
+++ hrmin.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+ 
+int main(void) {
+	int hour1,hour2,minit1,minit2,a;
+ 
+	printf("enter the hour and minit values");
+	scanf("%d%d",&hour1,&hour2,&minit1,&minit2);
+ 
+ 
+	a=hour1-hour2;
+	
+	return 0;

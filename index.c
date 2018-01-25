db702a041993ddcaf9a5bab08bdc56f924f7d2ac
nandhini191 <30830172+nandhini191@users.noreply.github.com>
2018-01-25 16:12:03 +0530

    Create index.c

---
+++ index.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+int number,index,i;
+int a[5];
+printf("enter the numbers");
+scanf("%d",number);
+for(i=0;i<=number;i++)
+{
+printf("the number is not present");
+}
+else
+{
+printf("display the index values");
+}
+return 0;
+}

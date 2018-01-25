282b231cdb0da79cac37aefdea602fc5eb3b9a23
nandhini191 <30830172+nandhini191@users.noreply.github.com>
2018-01-25 16:35:26 +0530

    Create nolines.c

---
+++ nolines.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+ 
+int main()
+{
+	char a[100];
+	printf("enter the word");
+	scanf("%d",&a)
+	{
+		if(a="")
+		{
+			count++;
+		}
+		{
+			printf("print the no of words in that sentense");
+		}
+ 
+		}
+	
+	return 0;
+}

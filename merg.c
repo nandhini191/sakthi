94d301e48994eb2ef0256e174d5f4a3aa1c9f21f
nandhini191 <30830172+nandhini191@users.noreply.github.com>
2018-01-25 16:03:35 +0530

    Create merg.c

---
+++ merg.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+int a={1,2,3,4,5}
+int minimam,value,c,i;
+printf("enter the number ");
+scanf("%d",&number);
+for(i=0;i<number;i++)
+{
+if(i==min)
+{
+printf("the number is ":);
+}
+else
+{
+printf("the number is not present");
+}
+}

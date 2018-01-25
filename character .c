a64baaed13447fbbb4cebf7776873f3ead1cae43
nandhini191 <30830172+nandhini191@users.noreply.github.com>
2018-01-25 16:24:59 +0530

    Create character .c

---
+++ character .c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+ 
+int main(void) {
+	char a[100];
+	printf("enter the character ");
+	scanf("%d",&a);
+	for(i=0;i<=a;i++)
+	{
+		\if(num="")
+		{
+			count++;
+		}
+		else
+		{
+			printf("print the number of letters ");
+ 
+		}
+		}
+	}
+ 
+	
+	return 0;
+}

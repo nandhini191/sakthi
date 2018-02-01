b0eb35daf1817970a9614a9324bc69f23bc4c8ac
nandhini191 <30830172+nandhini191@users.noreply.github.com>
2018-02-01 08:01:20 +0000

    Create character.c

---
+++ character.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+    char a[100];
+    int i=0,f;
+    printf("enter the character");
+    scanf("%s",&a);
+    for(i=0;i<=a[100];i++)
+    {
+        if(i!=0)
+        {
+            f=f+1;
+            printf("print the numbers");
+        
+        }
+        else
+        {
+            return 0;
+        }
+        
+    
+    }
+
+    return 0;
+}

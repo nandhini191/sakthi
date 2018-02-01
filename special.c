31a61dc7117a1170e6bf5f327c70964cd5760e5a
nandhini191 <30830172+nandhini191@users.noreply.github.com>
2018-02-01 08:33:07 +0000

    Create special.c

---
+++ special.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+  char a[50];
+  int i,f;
+  printf("enter the character");
+  scanf("%s",&a);
+  for(i=0;i<=a[50];i++)
+  {
+      if(i!=0)
+      {
+          f=f+1;
+          printf("print the special character")
+      }
+  }
+
+    return 0;
+}

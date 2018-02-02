05a276a78534f5b143fff8356c312169c91c88c9
nandhini191 <30830172+nandhini191@users.noreply.github.com>
2018-02-02 14:13:24 +0530

    Create k.c

---
+++ k.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+char a[20];
+int number,i;
+printf("enter the string");
+scanf("%s",&a);
+printf("enter the number");
+scanf("%d",number);
+for(i=0;i<=a;i++)
+{
+printf("%s\n",a);
+}
+}
+
